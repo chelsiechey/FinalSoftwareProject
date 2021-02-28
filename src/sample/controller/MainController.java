8250e853508527c5ed942b782377fbb6b6837088
Chelsie Conrad <chelsiechey@gmail.com>
2021-02-28 14:52:16 -0700

    first commit

---
+++ src/sample/controller/MainController.java
@@ -0,0 +1,32 @@
+package sample.controller;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
+import javafx.fxml.Initializable;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+
+public class MainController implements Initializable {
+    private Stage stage;  // Stage required to display application.
+    private Parent scene;
+    public void addPart(ActionEvent event) throws IOException {
+        // Get event source from button
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        // Load resources from view directory
+        scene = FXMLLoader.load(getClass().getResource("AddPart.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
---
+++ src/sample/model/Inventory.java
@@ -0,0 +1,46 @@
+package sample.model;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
+/**
+ * This public class provides Inventory methods
+ */
+public class Inventory {
+    // list of Part objects
+    private static ObservableList<Part> allParts = FXCollections.observableArrayList();
+    // list of Product objects
+    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+
+    /**
+     * This method adds a new part to the ObservableList array allParts
+     * @param newPart The new Part object that will be added to the ObservableList array allParts
+     */
+    public static void addPart(Part newPart) {
+        allParts.add(newPart);
+    }
+
+    /**
+     * This method adds a new product to the ObservableList array allProducts
+     * @param newProduct The new Product object that will be added to the ObservableList array allProducts
+     */
+    public static void addProduct(Product newProduct) {
+        allProducts.add(newProduct);
+    }
+
+    /**
+     * This method returns the Part object with the matching ID
+     * @param partId The ID for the Part object that is being looked up
+     * @return Returns either the Part object with the matching ID or null if not found
+     */
+    public static Part lookupPart(int partId) {
+        // FIX ME! Add code to find Part by partID
+        System.out.println("Fix me - Inventory model. Add code to find partID.");
+        return null;
+    }
+
+    public static Product lookupProduct(int productID) {
+        // FIX ME! Add code to find Product by productID
+        System.out.println("Fix me - Inventory model. Add code to find productID.");
+        return null;
+    }
+}
